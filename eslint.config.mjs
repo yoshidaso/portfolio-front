dd34aee820837edfe43418553b02b737145e1b03
sota <hitoesouta@gmail.com>
2025-04-05 10:03:40 +0900

    add es-lint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,60 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginNext from "@next/eslint-plugin-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+      parserOptions: {
+        ecmaVersion: "latest",
+        tsconfigRootDir: ".",
+        project: ["./tsconfig.json"],
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      js,
+      "react-hooks": pluginReactHooks,
+      next: pluginNext,
+      "@typescript-eslint": tseslint,
+      react: pluginReact,
+      "jsx-a11y": pluginA11y,
+      "react-hooks": pluginReactHooks,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "standard-with-typescript",
+      "plugin:react/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:react/jsx-runtime",
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+    ],
+    rules: {
+      
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
